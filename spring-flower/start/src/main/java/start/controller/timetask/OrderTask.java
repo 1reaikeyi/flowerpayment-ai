1fd3433ef24059aea5c7505f93372b1cd6467f40
1reaikeyi <reaikeyi@qq.com>
2026-08-27 16:57:00 +0800

    admin/shop和user/shop完成，使用rsdis缓存+mysql协作

--- spring-flower/start/src/main/java/start/controller/timetask/OrderTask.java
+++ spring-flower/start/src/main/java/start/controller/timetask/OrderTask.java
@@ -19,7 +19,7 @@ public class OrderTask {
     @Autowired
     private FlowerOrderService orderService;
     /**
-     * 帮助处理订单
+     * 3小时一次检查鲜花保质期
      */
 //    @Scheduled 使用 6 字段 cron 表达式（秒 分 时 日 月 周）
 //  1	秒	0-59	*任意 / */n每隔 n 秒
@@ -28,22 +28,9 @@ public class OrderTask {
 //  4	日	1-31	*任意日期
 //  5	月	1-12	*任意月份
 //  6	周	1 (周日)-7 (周六)	? 不指定（和日互斥）
-    //每60分钟触发一次
-    @Scheduled(cron = "0 0 * * * ?")
-    public void processTimeoutOrder(){
-        List<FlowerOrder> ordersList1 = orderService.lambdaQuery()
-                .eq(FlowerOrder::getStatus, OrderStatusEnum.GO).list();
-        if (!ordersList1.isEmpty()) {
-            log.info("有待处理订单：{}", ordersList1);
-        }
-//        List<FlowerOrder> ordersList2 = orderService.lambdaQuery()
-//                .eq(FlowerOrder::getDeliveryStatusEnum, DeliveryStatusEnum.NOW).list();
-//        for (FlowerOrder orders : ordersList2) {
-//            LocalDateTime now = LocalDateTime.now();
-//            if (orders.getStartDeliveryTime() != null && now.isAfter(orders.getStartDeliveryTime())) {
-//                    log.info("id为{}订单需要开始派送了", orders.getId());
-//            }
-//        }
+    //每3小时触发一次
+    @Scheduled(cron = "0 0 3 * * ?")
+    public void processTimeout(){
 
     }
 }
